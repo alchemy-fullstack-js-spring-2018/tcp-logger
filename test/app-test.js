fdf06b772766c9f775c00ad741934b01cff185dd
Ryan Manro <rmanro@gmail.com>
2018-04-09 22:50:18 -0700

    client writing to log most of the time as of right now

--- test/app-test.js
+++ test/app-test.js
@@ -9,29 +9,21 @@ describe('E2E', () => {
     const logFilePath = './log.txt';
     const server = app(logFilePath);
 
-    beforeEach(() => {
-        server.listen(PORT);
-    });
-
     let client1 = null;
     beforeEach(done => {
+        server.listen(PORT);
         client1 = net.connect(PORT, () => {
-            client1.write('please work');
             done();
         });
     });
 
     afterEach(() => {
         server.close();
-    });
-
-    afterEach(() => {
         client1.destroy();
     });
 
     it('test', () => {
-        
-        console.log('got here');
+        client1.write('please work');
     });
 
 });
