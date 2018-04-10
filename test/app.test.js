a8708281c82fdbaec1d4781ed792b73fd28c0e66
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-10 10:17:06 -0700

    specify tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 const app = require('../lib/app');
 const net = require('net');
 const assert = require('assert');
-const serverStart = require('../server');
+// const serverStart = require('../server');
 
 describe('e2e test', () => {
 
@@ -14,7 +14,6 @@ describe('e2e test', () => {
     let client1 = null;
     beforeEach(done => {
         client1 = net.connect(PORT, () => {
-            console.log('new connection on ', PORT);
             client1.setEncoding('utf8');
             done();
         });
@@ -28,6 +27,14 @@ describe('e2e test', () => {
         });
     });
 
+    let client3 = null;
+    beforeEach(done => {
+        client3 = net.connect(PORT, () => {
+            client3.setEncoding('utf8');
+            done();
+        });
+    });
+
 
     afterEach (() => {
         app.close();
@@ -36,12 +43,16 @@ describe('e2e test', () => {
     afterEach(() => {
         client1.destroy();
         client2.destroy();
+        client3.destroy();
     });
-
+    it('server is connected', () => {
+        assert.equal(app.listening, true);
+    });
     it('server connected and writes message', () => {
+
         const message = 'hello there';
         const written = client1.write(message);
-
+        client2.write('maybe');
         assert.equal(written, true);
     });
 
