17869bd89d80635b11f0be16abdd0bf6e2270815
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-10 12:35:00 -0700

    re-pushing

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const server = createServer(logFilePath);
 server.on('listening', () => {
     console.log('New server listening', PORT);
 });
+
 //once client creates socket connection write to log hello.
 
    
