1c884c1b78fbd09f1201433bad4242fcae262bf1
pereztjacob <pereztjacob@gmail.com>
2018-04-12 17:37:07 -0700

    functioning on data finally

--- test/server.test.js
+++ test/server.test.js
@@ -6,6 +6,6 @@ describe('server', () => {
     it('runs server', () => {
         const testServer = new GoServer();
         testServer.serverGo();
-        assert.equal(testServer.serverMessage, `TCP listening on port: ${testServer.PORT}`);
+        assert.equal(`${testServer.serverMessage}: ${testServer.PORT}`, `TCP listening on port: ${testServer.PORT}`);
     });
 });
